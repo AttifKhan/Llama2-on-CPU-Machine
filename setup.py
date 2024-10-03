718fa4e0f90fb1db18cf67d0083cf1e043802666
AttifKhan <kk1527218@gmail.com>
2024-10-03 22:36:48 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Attif Khan',
+    author_email= 'kk1527218@ineuron.ai',
+    packages= find_packages(),
+    install_requires = []
+
+)
